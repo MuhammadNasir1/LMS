b0d88f210d34edf1c625a6cede616ea1b8afb602
MuhammadNasir1 <nmian7080@gmail.com>
2024-04-06 13:22:12 +0500

    fix bug

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -396,12 +396,10 @@
     }
 </script>
 
-<script>
-    < script src = "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.min.js"
+    <script src = "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.min.js"
     integrity = "sha512-L0Shl7nXXzIlBSUUPpxrokqq4ojqgZFQczTYlGjzONGTDAcLremjwaWv5A+EDLnxhQzY5xUZPWLOLqYRkY0Cbw=="
     crossorigin = "anonymous"
     referrerpolicy = "no-referrer" >
-</script>
 </script>
 
 @include('layouts.footer')
