828e3d478b5581847f96182dde3852e26aa65f9c
MuhammadNasir1 <nmian7080@gmail.com>
2024-04-15 16:36:55 +0500

    add modal

--- resources/views/video/startvideo.blade.php
+++ resources/views/video/startvideo.blade.php
@@ -107,10 +107,10 @@
     </div>
 
 
-    <div class="mt-8 mb-8  flex  justify-center">
+    <div class="mt-8 mb-6  flex  justify-center">
         <div class="flex items-center  gap-5">
-            <button
-                class=" bg-secondary px-4 rounded-md h-12 text-white font-semibold text-xl">@lang('lang.Teaching_Page')</button>
+            <a href="../teacher/teachingpage"> <button
+                    class=" bg-secondary px-4 rounded-md h-12 text-white font-semibold text-xl">@lang('lang.Teaching_Page')</button></a>
             <button
                 class=" bg-secondary px-4 rounded-md h-12 text-white font-semibold text-xl">@lang('lang.Games')</button>
             <button id="recordButton"
@@ -125,16 +125,65 @@
                         fill="white" />
                 </svg>
                 <span id="recordBtnText">@lang('lang.Start_Recording')</span></button>
-            <button
-                class=" bg-secondary px-4 rounded-md h-12 text-white font-semibold text-xl">@lang('lang.Restart')</button>
+            {{-- <button
+                class=" bg-secondary px-4 rounded-md h-12 text-white font-semibold text-xl">@lang('lang.Restart')</button> --}}
         </div>
     </div>
-    <div id="fileInputContainer">
+
+
+</section>
+
+{{-- recording modal btn   --}}
+<button data-modal-target="recordingDel" data-modal-toggle="recordingDel" id="recordingModalBtn"
+    class="invisible absolute"> add recording btn
+</button>
+<div id="recordingDel" data-modal-backdrop="static"
+    class="hidden overflow-y-auto overflow-x-hidden fixed  left-0 z-50 justify-center  w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative w-full max-w-5xl max-h-full bg-white shadow-dark ">
+        <form id="teacherRec" method="post">
+            @csrf
+            <div id="fileInputContainer" class="invisible  absolute">
+
+            </div>
+            <video id="recordedVideo" src="" height="700px" width="100%" controls></video>
+            <a id="downloadLink" class=" absolute right-2 mt-3 mr-3 cursor-pointer" style="display: none"> <svg
+                    width="30px" xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 551 551"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                    <path
+                        d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V274.7l-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7V32zM64 352c-35.3 0-64 28.7-64 64v32c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V416c0-35.3-28.7-64-64-64H346.5l-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352H64zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z" />
+                </svg></a>
+            <div class="flex  mt-7  pb-6  justify-between items-end">
+                <div class="w-[80%] px-4 flex  items-start gap-5">
+                    <label for=""> @lang('lang.Teacher_comment')</label>
+                    <textarea name="" id="" class="h-28 w-[70%] rounded-md  border-gray focus:border-black"
+                        placeholder="@lang('lang.Enter_Comment')"></textarea>
+                </div>
+                <button class="bg-secondary text-white py-2 px-8 my-4 rounded-[4px]    mx-6  font-semibold">
+                    <div class=" text-center hidden" id="spinner">
+                        <svg aria-hidden="true"
+                            class="w-5 h-5 mx-auto text-center text-gray-200 animate-spin fill-primary"
+                            viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                fill="currentColor" />
+                            <path
+                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                fill="currentFill" />
+                        </svg>
+                    </div>
+                    <div id="text">
+                        @lang('lang.save')
+                    </div>
+
+                </button>
+            </div>
+        </form>
+        <div>
+
+        </div>
 
     </div>
-    <video id="recordedVideo" controls width="200px"></video>
-    <a id="downloadLink" style="display: none">Download Recorded Video</a>
-</section>
+</div>
 @include('layouts.footer')
 <script src="https://cdn.WebRTC-Experiment.com/RecordRTC.js"></script>
 <script>
@@ -142,6 +191,7 @@
     let audioStream;
     let recorder;
     let isRecording = false;
+    const recordingModalBtn = document.getElementById("recordingModalBtn");
     const recordButton = document.getElementById("recordButton");
     const recordBtnText = document.getElementById("recordBtnText");
     const recordedVideo = document.getElementById("recordedVideo");
@@ -199,6 +249,7 @@
             const fileInput = document.createElement("input");
             fileInput.type = "file";
             fileInput.accept = "video/webm";
+            fileInput.name = "teacherRecording";
             fileInput.multiple = false;
 
             const dataTransfer = new DataTransfer();
@@ -210,7 +261,7 @@
             isRecording = false;
             recordBtnText.textContent = "Start Recording";
         });
-
+        recordingModalBtn.click();
         tabStream.getTracks().forEach((track) => track.stop());
         audioStream.getTracks().forEach((track) => track.stop());
     }
